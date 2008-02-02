f4d9326521fc40ac94c2fa194e15fbe13b310a12
Laurent Montel <montel@kde.org>
2008-02-02 12:51:19 +0000

    Don't launch khelpcenter when X-DocPath is empty
    svn path=/trunk/KDE/kdebase/workspace/; revision=769928

--- kcmultiwidget.cpp
+++ kcmultiwidget.cpp
@@ -172,7 +172,8 @@ void KCMultiWidget::slotOk()
 void KCMultiWidget::slotHelp()
 {
     QString docPath = currentModule()->moduleInfo().docPath();
-
+    if(docPath.isEmpty())
+       return;
     KUrl url( KUrl("help:/"), docPath );
 
     if (url.protocol() == "help" || url.protocol() == "man" || url.protocol() == "info") {
