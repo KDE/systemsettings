c9574a5194b303ff123ca871fdfb90902f6ea825
Aaron J. Seigo <aseigo@kde.org>
2010-05-14 21:20:49 +0000

    center the tooltips on the items if possible, give a bit of breathing room
    svn path=/trunk/KDE/kdebase/workspace/; revision=1126782

--- app/ToolTips/tooltipmanager.cpp
+++ app/ToolTips/tooltipmanager.cpp
@@ -133,45 +133,47 @@ void ToolTipManager::showToolTip( QModelIndex menuItem )
     if (QApplication::mouseButtons() & Qt::LeftButton) {
         return;
     }
-    
+
     QWidget * tip = createTipContent( menuItem );
-    
+
     // calculate the x- and y-position of the tooltip
     const QSize size = tip->sizeHint();
     const QRect desktop = QApplication::desktop()->screenGeometry( d->itemRect.bottomRight() );
-    
+
     // d->itemRect defines the area of the item, where the tooltip should be
     // shown. Per default the tooltip is shown in the bottom right corner.
     // If the tooltip content exceeds the desktop borders, it must be assured that:
     // - the content is fully visible
     // - the content is not drawn inside d->itemRect
-    const bool hasRoomToLeft  = (d->itemRect.left()   - size.width()  >= desktop.left());
-    const bool hasRoomToRight = (d->itemRect.right()  + size.width()  <= desktop.right());
-    const bool hasRoomAbove   = (d->itemRect.top()    - size.height() >= desktop.top());
-    const bool hasRoomBelow   = (d->itemRect.bottom() + size.height() <= desktop.bottom());
+    const int margin = 3;
+    const bool hasRoomToLeft  = (d->itemRect.left()   - size.width() - margin >= desktop.left());
+    const bool hasRoomToRight = (d->itemRect.right()  + size.width() + margin <= desktop.right());
+    const bool hasRoomAbove   = (d->itemRect.top()    - size.height() - margin >= desktop.top());
+    const bool hasRoomBelow   = (d->itemRect.bottom() + size.height() + margin <= desktop.bottom());
     if (!hasRoomAbove && !hasRoomBelow && !hasRoomToLeft && !hasRoomToRight) {
         delete tip;
         tip = 0;
         return;
     }
-    
+
     int x = 0;
     int y = 0;
     if (hasRoomBelow || hasRoomAbove) {
-        x = QCursor::pos().x() + 16; // TODO: use mouse pointer width instead of the magic value of 16
-        if (x + size.width() >= desktop.right()) {
+        x = qMax(desktop.left(), d->itemRect.center().x() - size.width() / 2);
+        if (x + size.width() / 2 >= desktop.right()) {
             x = desktop.right() - size.width();
         }
-        y = hasRoomBelow ? d->itemRect.bottom() : d->itemRect.top() - size.height();
+
+        y = hasRoomBelow ? d->itemRect.bottom() + margin : d->itemRect.top() - size.height() - margin;
     } else {
         Q_ASSERT(hasRoomToLeft || hasRoomToRight);
-        x = hasRoomToRight ? d->itemRect.right() : d->itemRect.left() - size.width();
-        
+        x = hasRoomToRight ? d->itemRect.right() + margin : d->itemRect.left() - size.width() - margin;
+
         // Put the tooltip at the bottom of the screen. The x-coordinate has already
         // been adjusted, so that no overlapping with d->itemRect occurs.
         y = desktop.bottom() - size.height();
     }
-    
+
     // the ownership of tip is transferred to KToolTip
     KToolTip::showTip(QPoint(x, y), tip);
 }
