6a9df821ecfaa008206a260b20f595136e254821
Marco Martin <notmart@gmail.com>
2018-04-26 00:20:41 +0200

    QML applets need to manage padding by themselves
    Summary:
    remove our padding from qml kcms
    this fixes the cutted away shadow for InlineMessage
    
    Test Plan: InlineMessages work better now
    
    Reviewers: #plasma, hein
    
    Reviewed By: #plasma, hein
    
    Subscribers: hein, abetts, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D12515

--- core/ModuleView.cpp
+++ core/ModuleView.cpp
@@ -348,6 +348,21 @@ void ModuleView::activeModuleChanged(KPageWidgetItem * current, KPageWidgetItem 
     if (activeModule) {
         KActivities::ResourceInstance::notifyAccessed(QUrl(QStringLiteral("kcm:") + activeModule->moduleInfo().service()->storageId()),
                 QStringLiteral("org.kde.systemsettings"));
+        if (activeModule->realModule() && activeModule->realModule()->inherits("KCModuleQml")) {
+            d->mButtons->setContentsMargins(
+                style()->pixelMetric(QStyle::PM_LayoutLeftMargin),
+                style()->pixelMetric(QStyle::PM_LayoutTopMargin),
+                style()->pixelMetric(QStyle::PM_LayoutRightMargin),
+                style()->pixelMetric(QStyle::PM_LayoutBottomMargin));
+            d->mLayout->setContentsMargins(0, 0, 0, 0);
+        } else {
+            d->mButtons->setContentsMargins(0, 0, 0, 0);
+            d->mLayout->setContentsMargins(
+                style()->pixelMetric(QStyle::PM_LayoutLeftMargin),
+                style()->pixelMetric(QStyle::PM_LayoutTopMargin),
+                style()->pixelMetric(QStyle::PM_LayoutRightMargin),
+                style()->pixelMetric(QStyle::PM_LayoutBottomMargin));
+        }
     }
 }
 
