331d72f1a1ccf537a0f8f8686b1a41bd0df01990
Alexander Potashev <aspotashev@gmail.com>
2021-01-04 10:26:21 +0100

    Revert "Revert "Revert "test"""
    This reverts commit 3af9a436e750ca1986c49c2ed02c870c562a187f.

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
-
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/systemsettings.pot
 rm -f rc.cpp
