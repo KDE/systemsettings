20b6ad504cafffefe398eebd6026de84d91dba5b
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-10-07 19:25:59 +0200

    Add CC0-1.0 license identifier to non-copyrightable files
    This will help making the project reuse compliant

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
+
+# SPDX-FileCopyrightText: none
+# SPDX-License-Identifier: CC0-1.0
+
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/systemsettings.pot
 rm -f rc.cpp
